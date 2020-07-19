05a2b59e581f109db2a81ed3472fdc2ecd43d67e
deepanshu102 <deepanshujindal1021997@gmail.com>
2020-07-20 00:21:15 +0530

    default package

---
+++ src/main/java/com/niit/indigo/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.niit.indigo;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CustomersApplication.class);
+	}
+
+}
